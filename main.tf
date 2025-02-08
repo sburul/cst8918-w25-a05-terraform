594366ad6fbf5b5fdc15f36e4fa39e3d81e28cfc
buru0003 <buru0003@algonquinlive.com>
2025-02-07 20:57:29 -0500

    Define network security group and rules

--- main.tf
+++ main.tf
@@ -73,3 +73,35 @@ resource "azurerm_subnet" "webserver" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+# Define network security group and rules
+resource "azurerm_network_security_group" "webserver" {
+  name                = "${var.labelPrefix}A05SG"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
