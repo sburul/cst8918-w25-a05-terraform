b788ac83e93d0831173fed224b957bea09346f79
buru0003 <buru0003@algonquinlive.com>
2025-02-07 20:55:36 -0500

    Define a public IP

--- main.tf
+++ main.tf
@@ -49,3 +49,11 @@ resource "azurerm_resource_group" "rg" {
   name     = "${var.labelPrefix}-A05-RG"
   location = var.region
 }
+
+# Define a public IP address
+resource "azurerm_public_ip" "webserver" {
+  name                = "${var.labelPrefix}A05PublicIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
