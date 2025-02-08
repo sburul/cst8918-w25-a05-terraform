78db443306df1326f919495905db53f5235c062b
buru0003 <buru0003@algonquinlive.com>
2025-02-07 20:56:48 -0500

    Define the subnet

--- main.tf
+++ main.tf
@@ -65,3 +65,11 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+# Define the subnet
+resource "azurerm_subnet" "webserver" {
+  name                 = "${var.labelPrefix}A05Subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
