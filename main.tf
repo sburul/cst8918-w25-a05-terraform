ffb6d29ac8ea8ebe9b2cf89adc418a0e8f8b3f20
buru0003 <buru0003@algonquinlive.com>
2025-02-07 20:56:17 -0500

    Define the virtual network

--- main.tf
+++ main.tf
@@ -57,3 +57,11 @@ resource "azurerm_public_ip" "webserver" {
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
 }
+
+# Define the virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.labelPrefix}A05Vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
