62e577a86d766278febadabc13a8d30474eda31f
buru0003 <buru0003@algonquinlive.com>
2025-02-07 21:03:34 -0500

    Define output values for later reference

--- main.tf
+++ main.tf
@@ -170,3 +170,12 @@ resource "azurerm_linux_virtual_machine" "webserver" {
 
   custom_data = data.cloudinit_config.init.rendered
 }
+
+# Define output values for later reference
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "public_ip" {
+  value = azurerm_linux_virtual_machine.webserver.public_ip_address
+}
